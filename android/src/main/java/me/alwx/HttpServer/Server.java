27fb4ebed2b2a79c08e630402bb8013e753452e1
Ivan Rufino Martins <ivan.rufino.m@gmail.com>
2020-12-10 10:37:01 -0300

    Update Server.java

--- android/src/main/java/me/alwx/HttpServer/Server.java
+++ android/src/main/java/me/alwx/HttpServer/Server.java
@@ -71,6 +71,7 @@ public class Server extends NanoHTTPD {
         WritableMap request = Arguments.createMap();
         request.putString("url", session.getUri());
         request.putString("type", method.name());
+        request.putString("params", session.getParms());
         request.putString("requestId", requestId);
         
         Map<String, String> files = new HashMap<>();
